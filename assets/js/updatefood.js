0e30ea0877dcff1e3f2228f0b435b55ed608c739
Phil <philipp@wornath.de>
2019-02-20 14:26:22 +0100

    Beta_cleanup

--- assets/js/updatefood.js
+++
@@ -1,8 +0,0 @@
-function updatefood() {
-    if (foodTokens >= 0) {
-        currentFood = randomarray(food);
-        foodTokens -= 1;
-        checktokens();
-        updatespacecraftvalues();
-    }
-}
